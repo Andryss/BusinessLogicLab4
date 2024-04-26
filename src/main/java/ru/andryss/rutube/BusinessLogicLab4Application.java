a7448eeb0e5071de1ed7b630169f6ef14cbb5dcf
Andryss <andr.krivosheev@gmail.com>
2024-04-26 20:01:44 +0300

    Initial commit

---
+++ src/main/java/ru/andryss/rutube/BusinessLogicLab4Application.java
@@ -0,0 +1,13 @@
+package ru.andryss.rutube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessLogicLab4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessLogicLab4Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/andryss/rutube/BusinessLogicLab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andryss.rutube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessLogicLab4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
