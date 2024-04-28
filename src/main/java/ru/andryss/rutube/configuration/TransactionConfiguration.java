d072d197b2a6ff7f5f7cc67ed2c7e86e9d052e5c
Andryss <andr.krivosheev@gmail.com>
2024-04-28 13:13:54 +0300

    Add transaction management

---
+++ src/main/java/ru/andryss/rutube/configuration/TransactionConfiguration.java
@@ -0,0 +1,30 @@
+package ru.andryss.rutube.configuration;
+
+import com.atomikos.icatch.jta.UserTransactionImp;
+import com.atomikos.icatch.jta.UserTransactionManager;
+import com.atomikos.spring.AtomikosDataSourceBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.jta.JtaTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableRetry
+public class TransactionConfiguration {
+
+    @Bean
+    public DataSource camundaBpmDataSource() {
+        return new AtomikosDataSourceBean();
+    }
+
+    @Bean
+    public PlatformTransactionManager camundaBpmTransactionManager() {
+        JtaTransactionManager transactionManager = new JtaTransactionManager();
+        transactionManager.setUserTransaction(new UserTransactionImp());
+        transactionManager.setTransactionManager(new UserTransactionManager());
+        return transactionManager;
+    }
+}
