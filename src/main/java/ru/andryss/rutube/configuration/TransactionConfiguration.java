274e4a18cddef92adc1b4756d63cb08d54fbded3
Andryss <andr.krivosheev@gmail.com>
2024-05-01 12:50:32 +0300

    Add distributed atomikos transactions

--- src/main/java/ru/andryss/rutube/configuration/TransactionConfiguration.java
+++ src/main/java/ru/andryss/rutube/configuration/TransactionConfiguration.java
@@ -1,10 +1,16 @@
 package ru.andryss.rutube.configuration;
 
+import com.atomikos.icatch.jta.TransactionSynchronizationRegistryImp;
+import com.atomikos.icatch.jta.UserTransactionImp;
+import com.atomikos.icatch.jta.UserTransactionManager;
+import com.atomikos.spring.AtomikosDataSourceBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.annotation.EnableRetry;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.jta.JtaTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
+
+import javax.sql.DataSource;
 
 import static org.springframework.transaction.TransactionDefinition.*;
 
@@ -13,18 +19,33 @@ import static org.springframework.transaction.TransactionDefinition.*;
 public class TransactionConfiguration {
 
     @Bean
-    public TransactionTemplate transactionTemplate(PlatformTransactionManager transactionManager) {
-        TransactionTemplate template = new TransactionTemplate(transactionManager);
+    public TransactionTemplate transactionTemplate() {
+        TransactionTemplate template = new TransactionTemplate(transactionManager());
         template.setIsolationLevel(ISOLATION_REPEATABLE_READ);
         template.setPropagationBehavior(PROPAGATION_REQUIRES_NEW);
         return template;
     }
 
     @Bean
-    public TransactionTemplate readOnlyTransactionTemplate(PlatformTransactionManager transactionManager) {
-        TransactionTemplate template = new TransactionTemplate(transactionManager);
+    public TransactionTemplate readOnlyTransactionTemplate() {
+        TransactionTemplate template = new TransactionTemplate(transactionManager());
         template.setIsolationLevel(ISOLATION_READ_COMMITTED);
         template.setPropagationBehavior(PROPAGATION_REQUIRES_NEW);
         return template;
+    }
+
+    @Bean
+    public JtaTransactionManager transactionManager() {
+        JtaTransactionManager jtaTransactionManager = new JtaTransactionManager();
+        jtaTransactionManager.setUserTransaction(new UserTransactionImp());
+        jtaTransactionManager.setTransactionManager(new UserTransactionManager());
+        jtaTransactionManager.setTransactionSynchronizationRegistry(new TransactionSynchronizationRegistryImp());
+        jtaTransactionManager.setAllowCustomIsolationLevels(true);
+        return jtaTransactionManager;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new AtomikosDataSourceBean();
     }
 }
